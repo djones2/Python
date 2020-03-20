1ad9c2b6e1216665317dff6fe3f5bb84a7ff82cc
Daniel <dcjones2015@gmail.com>
2020-03-20 12:00:02 -0700

    Folders

---
+++ Sorting/mergeSort.py
@@ -0,0 +1,12 @@
+import sys
+
+""" 
+A quick example of performing merge sort on an array.
+"""
+
+def mergeSort(input):
+    print("Here")
+
+# Main Program 
+if __name__ == "__main__":
+    val = input("Enter a value: ")
